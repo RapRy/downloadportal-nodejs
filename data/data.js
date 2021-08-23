8551fdbab7266d17a47a3c5bb25756396f435c6d
Ryan <ry.quickcell@gmail.com>
2021-08-23 16:08:53 +0800

    add subcategory and search route

--- data/data.js
+++ data/data.js
@@ -64,8 +64,67 @@ const fetches = [
       `,
   },
   {
+    heading: "Fetch Contents By Subcategory",
+    link: "https://downloadstoreportal.herokuapp.com/contents/Games/Arcade",
+    output: `
+        {
+            contents:
+                [
+                    {
+                        _id: String,
+                        name: String,
+                        catName: String,
+                        subCatName: String,
+                        thumbnail: String
+                      },
+                      { ... },
+                      { ... }
+                ]     
+        }
+    `,
+  },
+  {
+    heading: "Fetch Contents Via Search",
+    link: "https://downloadstoreportal.herokuapp.com/contents/search",
+    parameters: [
+      {
+        type: "required",
+        params: [
+          {
+            key: "keyword",
+            values: [
+              {
+                value: "user input",
+                desc: "search content collection based from the user input",
+                example:
+                  "https://downloadstoreportal.herokuapp.com/contents/search?keyword=user input",
+              },
+            ],
+            defaultValue: "undefined",
+          },
+        ],
+      },
+    ],
+    output: `
+      {
+        contents: 
+            [
+              {
+                _id: String,
+                name: String,
+                catName: String,
+                subCatName: String,
+                thumbnail: String
+              },
+              { ... },
+              { ... }
+            ]
+      }
+      `,
+  },
+  {
     heading: "Fetch Featured Contents",
-    link: "https://downloadstoreportal.herokuapp.com/contents/featured",
+    link: "https://downloadstoreportal.herokuapp.com/contents/search",
     output: `
       {
         contents:
@@ -114,6 +173,8 @@ const fetches = [
 const routes = [
   { method: "get", route: "/categories" },
   { method: "get", route: "/contents/name of category" },
+  { method: "get", route: "/contents/name of category/name of subcategory" },
+  { method: "get", route: "/contents/search?keyword=user input" },
   { method: "get", route: "/contents/featured" },
   { method: "get", route: "/contents/details/id of content" },
 ];
